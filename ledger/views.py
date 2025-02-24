d09b2767473934e19aff8d0a9ae515075ac6b72c
David Tormon <davidtormon@gmail.com>
2025-02-24 23:46:03 +0800

    fixed recipeList context

--- ledger/views.py
+++ ledger/views.py
@@ -76,9 +76,67 @@ def recipe2(request):
 
 def recipeList(request):
     ctx = {
-        "recipes": [
-            "Recipe 1",
-            "Recipe 2"
-        ]
-    }
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
     return render(request, "recipeList.html", ctx)
