b26a1520b0ce3e0b646e6d9d2459a2a145e95340
David Tormon <davidtormon@gmail.com>
2025-02-24 23:48:14 +0800

    added links to recipeList page

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,8 +5,8 @@ from .views import home
 urlpatterns = [
     path('', home, name = "home"),
     path("recipes/list", views.recipeList, name = "recipeList"),
-    path("recipes/1", views.recipe1, name = "recipe1"),
-    path("recipes/2", views.recipe2, name = "recipe2")
+    path("recipe/1", views.recipe1, name = "recipe1"),
+    path("recipe/2", views.recipe2, name = "recipe2")
 ]
 
 app_name = "ledger"
