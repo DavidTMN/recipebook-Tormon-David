5626cf371e7567bfe2a6dab5604d4a95bde7f786
David Tormon <davidtormon@gmail.com>
2025-03-06 18:21:31 +0800

    added classes Task and TaskGroup to models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    isCompleted = models.BooleanField(default=False)
+    dateCreated = models.DateTimeField(null=False)
+    taskgroup = models.ForeignKey(TaskGroup, on_delete=models.CASCADE)
