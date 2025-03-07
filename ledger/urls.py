0e0266fdd065299bbaed5ee9c3bf97bf17985350
David Tormon <davidtormon@gmail.com>
2025-03-07 21:51:55 +0800

    fixed urls syntax

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,7 +4,7 @@ from .views import home
 
 urlpatterns = [
     path('', home, name = "home"),
-    path("recipes/list", views.recipesinDatabase, name = "recipeList"),
+    path("recipes/list", views.recipesInDatabase, name = "recipeList"),
     path("recipe/1", views.recipe1, name = "recipe1"),
     path("recipe/2", views.recipe2, name = "recipe2"),
 ]
