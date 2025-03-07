eac3286b21608e57497646d299b29e9755d32b85
David Tormon <davidtormon@gmail.com>
2025-03-07 22:08:16 +0800

    added list_display to admin models

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
 
 # Register your models here.
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+    list_display = ("name",)
     
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    list_display = ("name",)
     
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+    list_display = ("name",)
