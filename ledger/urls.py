e7e4c786a878b5e3acce24b7cc7c34998d600388
David Tormon <davidtormon@gmail.com>
2025-03-21 21:42:52 +0800

    added path for account login or logout

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,5 +5,6 @@ from .views import recipesInDatabase, recipeContents
 urlpatterns = [
     path("recipes/list", views.recipesInDatabase, name = "recipeList"),
     path("recipe/<int:pk>", views.recipeContents, name = "recipeDetail"),
+    path("/accounts," include("django.contrib.auth.urls"))
 ]
 app_name = "ledger"
