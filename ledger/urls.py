91eba4e39219104577eb7cb3f334eedf4451e4b2
David Tormon <davidtormon@gmail.com>
2025-03-21 23:24:57 +0800

    added paths for login and logout

--- ledger/urls.py
+++ ledger/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path("recipes/list", views.recipesInDatabase, name = "recipeList"),
     path("recipe/<int:pk>", views.recipeContents, name = "recipeDetail"),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/login")
+    path("accounts/login", views.recipesInDatabase, name = "login")
+    path("accounts/logout", views.recipesInDatabase, name = "logout")
+    
     
 ]
 app_name = "ledger"
