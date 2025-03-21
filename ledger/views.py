dd8254876651fda04410ad0152ebbcc067e67cf9
David Tormon <davidtormon@gmail.com>
2025-03-21 21:38:50 +0800

    added view_function to views

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
 
+@login_required
+def view_function(request):
+    return render (request,)
 
 def recipesInDatabase(request):
     recipes = Recipe.objects.all()
