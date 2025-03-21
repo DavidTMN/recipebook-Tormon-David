c3a600d49a9d52c30ba8cd1a0caecc1441cac618
David Tormon <davidtormon@gmail.com>
2025-03-21 23:28:56 +0800

    undid changes to paths

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,10 +5,7 @@ from .views import recipesInDatabase, recipeContents
 urlpatterns = [
     path("recipes/list", views.recipesInDatabase, name = "recipeList"),
     path("recipe/<int:pk>", views.recipeContents, name = "recipeDetail"),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/login", views.recipesInDatabase, name = "login")
-    path("accounts/logout", views.recipesInDatabase, name = "logout")
-    
+    path("accounts/", include("django.contrib.auth.urls")),   
     
 ]
 app_name = "ledger"
