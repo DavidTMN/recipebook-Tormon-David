6eb18f7f32de54b7a66cac0bd3b7c2c2dbb2df11
David Tormon <davidtormon@gmail.com>
2025-03-21 23:40:20 +0800

    added if case to logout button

--- ledger/views.py
+++ ledger/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def view_function(request):
-    return render (request,)
-
 def recipesInDatabase(request):
     recipes = Recipe.objects.all()
     return render(request, "recipeList.html", {"recipes": recipes})
 
+@login_required
 def recipeContents(request, pk):
     recipe = Recipe.objects.get(pk=pk)
     return render(request, "ingredientList.html", {"recipe": recipe})
