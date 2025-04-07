96f36d9ae9a1b9423cd7e427cb5d667972f99711
David Tormon <davidtormon@gmail.com>
2025-04-07 21:47:12 +0800

    created Recipe_Add and renamed methods to follow PEP8

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from . import views
-from .views import RecipesInDatabase, RecipeContents, RecipeAdd
+from .views import Recipes_In_Database, Recipe_Contents, Recipe_Add
 
 urlpatterns = [
-    path('recipes/list/', views.RecipesInDatabase, name = 'RecipeList'),
-    path('recipe/<int:pk>/', views.RecipeContents, name = 'RecipeDetail'),
-    path('recipe/add/', views.RecipeAdd, name = 'RecipeAdd'),
+    path('recipes/list/', views.Recipes_In_Database, name = 'RecipeList'),
+    path('recipe/<int:pk>/', views.Recipe_Contents, name = 'RecipeDetail'),
+    path('recipe/add/', views.Recipe_Add, name = 'RecipeAdd'),
 ]
 app_name = 'ledger'
--- ledger/views.py
+++ ledger/views.py
@@ -3,15 +3,15 @@ from .models import Recipe
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def RecipesInDatabase(request):
+def Recipes_In_Database(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipes': recipes})
 
 @login_required
-def RecipeContents(request, pk):
+def Recipe_Contents(request, pk):
     recipe = Recipe.objects.get(pk=pk)
     return render(request, 'ingredient_list.html', {'recipe': recipe})
 
 @login_required
-def RecipeAdd(reqiest):
-    ()
+def Recipe_Add(request):
+    return render(request, 'recipe_add.html', )
