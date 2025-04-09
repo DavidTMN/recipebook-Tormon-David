b87ebf280c6104ed35a50122498b33e0a75effd6
David Tormon <davidtormon@gmail.com>
2025-04-09 23:02:41 +0800

    fixed add_recipe method

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Recipe
+from .models import Recipe, Profile
 from .forms import RecipeImageForm, RecipeForm
 
 @login_required
@@ -34,6 +34,10 @@ def add_recipe(request, pk):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
         if form.is_valid():
-            task = form.save()
-            return redirect('recipe_contents', pk=task.pk)
+            recipe = form.save(commit=False)
+            author = Profile.objects.get(user=request.user)
+            recipe.author = author
+            recipe.save()
+            return redirect('recipe_contents')
+    return render(request, "add_recipe.html", {"form": form})
             
