fe53f47df5f9c8d1a139f18180b70c49fb1ea2a0
David Tormon <davidtormon@gmail.com>
2025-04-09 23:44:29 +0800

    fixed paths and design

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,7 +5,7 @@ from .views import recipes_in_database, recipe_contents, add_image, add_recipe
 urlpatterns = [
     path('recipes/list/', views.recipes_in_database, name = 'RecipeList'),
     path('recipe/<int:pk>/', views.recipe_contents, name = 'RecipeDetail'),
-    path('recipe/add/', views.add_image, name = 'AddImage'),
+    path('recipe/<int:pk>/add_image/', views.add_image, name = 'AddImage'),
     path('recipe/add', add_recipe, name="AddRecipe")
 ]
 app_name = 'ledger'
